98e1b5bb597a80cef78e0bb71eaab132de9f0a8f
らいちちゃん <rusuke@t-net.ne.jp>
2026-06-06 18:18:17 +0900

    change config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,6 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
+  trailingSlash: false,
   title: 'Hanataba Sound Manager Docs',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
