7080960e6381c5c49affa36599d4a690eb21cdfb
らいちちゃん <rusuke@t-net.ne.jp>
2026-06-07 02:17:10 +0900

    disable dark mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,6 +59,7 @@ const config: Config = {
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       defaultMode: 'light',
+      disableSwitch: true,
       respectPrefersColorScheme: true,
     },
     navbar: {
