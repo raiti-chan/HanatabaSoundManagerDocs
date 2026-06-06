cdffb223e136980f7173a205a719ca1372509491
らいちちゃん <rusuke@t-net.ne.jp>
2026-06-07 02:14:19 +0900

    Create other component page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://raiti-chan.hsm.docs.hanataba.raitichan.net.io/',
+  url: 'https://hsm.docs.hanataba.raitichan.net/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
