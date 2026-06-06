662bee583773d109dfdc0ad4b5cb268673754064
らいちちゃん <rusuke@t-net.ne.jp>
2026-06-06 18:36:01 +0900

    fix config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: 'https://raiti-chan.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/HanatabaSoundManager/',
+  baseUrl: '/HanatabaSoundManagerDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
